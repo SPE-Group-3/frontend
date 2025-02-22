575a5a369058201a14c0c6def44a6a6325aa08dc
Kevin Osei <knokwaku@gmail.com>
2025-02-22 22:05:28 +0000

    picture changes

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,35 +5,36 @@ const Featured = () => {
     <div className="featured">
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          src="https://img.goodfon.com/wallpaper/big/d/68/alicante-alikante-valencia.webp"
+          
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
+          <h1>Alicante</h1>
           <h2>123 properties</h2>
         </div>
       </div>
       
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+          src="https://www.kayak.co.uk/rimg/himg/ab/8e/ae/ice-158816748-65153508_3XL-053511.jpg?width=1366&height=768&crop=true"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Singapore</h1>
-          <h2>533 properties</h2>
+          <h1>Glasgow</h1>
+          <h2>413 properties</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+          src="https://wallpapercave.com/wp/wp2163830.jpg"
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
+          <h1>mykonos</h1>
           <h2>232 properties</h2>
         </div>
       </div>
--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -19,12 +19,12 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
+          src="https://images.trvl-media.com/lodging/23000000/22450000/22448200/22448148/54b91e14.jpg?impolicy=resizecrop&rw=575&rh=575&ra=fill.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
           alt=""
           className="fpImg"
         />
-        <span className="fpName">Comfort Suites Airport</span>
-        <span className="fpCity">Austin</span>
+        <span className="fpName">shama lakeview</span>
+        <span className="fpCity">Bangkok</span>
         <span className="fpPrice">Starting from $140</span>
         <div className="fpRating">
           <button>9.3</button>
@@ -33,7 +33,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/232902339.jpg?k=3947def526b8af0429568b44f9716e79667d640842c48de5e66fd2a8b776accd&o=&hp=1"
+          src="https://res.klook.com/image/upload/fl_lossy.progressive,c_fill,f_auto,w_750,q_85/v1647582332/hotel/ycqlrojeek5b053pgmxs.jpg"
           alt=""
           className="fpImg"
         />
@@ -47,7 +47,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
+          src="https://images.trvl-media.com/lodging/1000000/30000/20300/20211/a03cdbdf.jpg?impolicy=resizecrop&rw=575&rh=575&ra=fill"
           alt=""
           className="fpImg"
         />
